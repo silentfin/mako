94a7a0e7a232241675611ff84e6bce2790b69f7d
silentfin <152957041+silentfin@users.noreply.github.com>
2026-04-17 09:21:49 +0530

    add broken auth

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import jwt
 from fastapi import FastAPI, HTTPException
 from jwt.exceptions import InvalidTokenError
 from pwdlib import PasswordHash
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 from db import get_connection, init_db
 
@@ -16,26 +16,59 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 5
 
-app = FastAPI()
-init_db()
-
-password_hash = PasswordHash.recommended()
-
-
-class User(BaseModel):
-    username: str
-    email: str
-    password: str
-
-
-class LoginRequest(BaseModel):
-    username: str
-    password: str
-
 
 class Token(BaseModel):
     access_token: str
     token_type: str
+
+
+class TokenData(BaseException):
+    username: str | None = None
+
+
+class User(BaseModel):
+    username: str
+    email: EmailStr
+    disabled: bool | None = None
+
+
+class NewUser(User):
+    password: str | None = None
+
+
+class UserInDB(User):
+    hashed_password: str
+
+
+app = FastAPI()
+init_db()
+
+password_hash = PasswordHash.recommended()
+DUMMY_HASH = password_hash.hash("dummypassword")
+
+
+def verify_password(plain_password, hashed_password):
+    return password_hash.verify(plain_password, hashed_password)
+
+
+def get_passwrod_hash(password):
+    return password_hash.hash(password)
+
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+
+def authenticate_user(fake_db, username: str, password: str):
+    user = get_user(fake_db, username)
+    if not user:
+        verify_password(password, DUMMY_HASH)
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
 
 
 def create_access_token(data: dict, expires_delta: timedelta | None = None):
@@ -49,55 +82,51 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
     return encoded_jwt
 
 
-#
-# @app.get("/")
-# def all_users():
-#     conn = get_connection()
-#     cursor = conn.cursor()
-#     cursor.execute("select * from users")
-#     rows = cursor.fetchall()
-#     if rows:
-#         links = {row["username"]: (row["email"], row["password_hash"]) for row in rows}
-#         conn.close()
-#         return links
-#     else:
-#         return "NOT FOUND!!"
-#
+@app.get("/")
+def all_users():
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("select * from users")
+    rows = cursor.fetchall()
+    if rows:
+        links = {row["username"]: (row["email"], row["password_hash"]) for row in rows}
+        conn.close()
+        return links
+    else:
+        return "NOT FOUND!!"
 
 
-@app.post("/auth/login")
-def user_login(user: LoginRequest):
+@app.post("/login")
+def user_login(user: UserInDB):
     conn = get_connection()
     cursor = conn.cursor()
     cursor.execute("select * from users where username = ?", (user.username,))
     row = cursor.fetchone()
     conn.close()
-    print(f"{user.username} is trying to login!!")
     if not row:
         raise HTTPException(status_code=401, detail="Invalid credentials")
-    if not password_hash.verify(user.password, row["password_hash"]):
+    print(f"{user.username} is trying to login!!")
+    if not password_hash.verify(user.hashed_password, row["password_hash"]):
         raise HTTPException(status_code=401, detail="Invalid credentials")
     print(f"{user.username} has logged in successfully!!")
     token = create_access_token({"sub": user.username})
-    # return {"SUCCESS!!": f"Welcome {user.username} !!!!"}
     return Token(access_token=token, token_type="bearer")
 
 
-@app.post("/auth/register")
-def add_user(user: User):
+@app.post("/register")
+def add_user(user: NewUser):
     print(f"Username: {user.username} is recieved!!!")
     print(f"Email: {user.email} is recieved!!!")
     print(f"Password: {user.password} is recieved!!!")
     conn = get_connection()
     cursor = conn.cursor()
     hashed_pass = password_hash.hash(user.password)
+    print(f"Hashed Password: {hashed_pass} is recieved!!!")
     cursor.execute("select username from users where username=?", (user.username,))
     row = cursor.fetchone()
     if row:
         conn.close()
-        raise HTTPException(
-            status_code=400, detail="username already exists! login instead"
-        )
+        raise HTTPException(status_code=400, detail="Username Not Available!")
     cursor.execute(
         "insert into users (username, email, password_hash) values (?,?,?)",
         (user.username, user.email, hashed_pass),
